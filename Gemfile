b21126dc9aa64007f080ccdeb1da57b0868281c4
James Hebden <james@hebden.net.au>
2016-08-15 12:14:46 +1000

    Added MPPT vars

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@
 source "https://rubygems.org"
 
 gem "bunny"
-gem "rubyserial"
+gem "serialport"
 
--- victronup.rb
+++ victronup.rb
@@ -39,16 +39,41 @@ while true do
       when 'MODE'
         update( 'mode', value )
       when 'AC_OUT_V'
-        update( 'acvolts', "#{ value.to_i / 100 }V" )
+        update( 'acvolts', "#{ value.to_f / 1000 }V" )
+      when 'VPV'
+        update( 'pvvolts', "#{ value.to_f / 1000 }V" )
       when 'AC_OUT_I'
 	update( 'acamps', "#{ value }A" )
       when 'V'
-	update( 'dcvolts', "#{ value.to_i / 1000 }V" )
+	update( 'dcvolts', "#{ value.to_f / 1000 }V" )
       when 'AR'
         update( "alarm", value )
+      when 'ERR'
+        update( "error", value )
+      when 'I'
+        update( "batteryamps", "#{ value }mA" )
+      when 'IL'
+        update( "loadamps", "#{ value }mA" )
+      when 'PPV'
+        update( "solarwatts", "#{ value }W" )
+      when 'LOAD'
+        update( "loadstatus", value )
+      when 'H19'
+        update( "yieldtotal", "#{ value.to_f / 100}kWh" ) 
+      when 'H20'
+        update( "yieldtoday", "#{ value.to_f / 100}kWh" )
+      when 'H22'
+        update( "yieldyesterday", "#{ value.to_f / 100}kWh" )
+      when 'H21'
+        update( "maxpowertoday", "#{ value }W" )
+      when 'H23'
+        update( "maxpoweryesterday", "#{ value }W" )
+      else
+        puts "Unknown field: #{ data }"
       end
     end
   end
+
 
   sleep 0.01
 end
